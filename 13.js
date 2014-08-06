27a6b2bf950c8cb64d3e976dcaec693366e50a31
jnozsc <jnozsc@gmail.com>
2014-08-06 16:34:15 -0400

    typo

--- 13.js
+++ 13.js
@@ -1,5 +1,5 @@
 var http = require('http')
-       var portNumber = Number(process.argv[2])
+var portNumber = Number(process.argv[2])
 
 var server = http.createServer(function(req, res) {
   if (req.method == 'GET') {
